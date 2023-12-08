035ae2e5d616b2948d7292b59d5180583754cfed
Dex <maroderko8@gmail.com>
2023-12-08 10:46:52 +0200

    Initial commit

---
+++ src/main/kotlin/com/dex/anotheronemicroblog/AnotherOneMicroBlogApplication.kt
@@ -0,0 +1,11 @@
+package com.dex.anotheronemicroblog
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AnotherOneMicroBlogApplication
+
+fun main(args: Array<String>) {
+    runApplication<AnotherOneMicroBlogApplication>(*args)
+}
---
+++ src/test/kotlin/com/dex/anotheronemicroblog/AnotherOneMicroBlogApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dex.anotheronemicroblog
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AnotherOneMicroBlogApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
